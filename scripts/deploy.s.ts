fd780d232b93f6ee8f223cd324ca4e7d2abac79b
Sergey <mysteryon88@yandex.ru>
2024-05-04 17:11:49 +0300

    fix front

--- scripts/deploy.s.ts
+++ scripts/deploy.s.ts
@@ -1,5 +1,7 @@
 import hre from "hardhat";
 
+// npx hardhat run scripts/deploy.s.ts --network localhost
+
 async function main() {
   const SBT = await hre.ethers.getContractFactory("Soulbound");
   const sbt = await SBT.deploy("test", "test");
@@ -11,8 +13,16 @@ async function main() {
     "https://ipfs.io/ipfs/QmaNMk641puZy1uth85UCM4MZiXB9qUyuverkBo5bPu35n"
   );
 
+  await sbt.setURI(
+    "1",
+    "https://ipfs.io/ipfs/Qmd11ZPoHtHX7pJA3LWM3rRieiW3gptPoksRPD6zsY5XyK/29.json"
+  );
+
   await sbt.mint("0x4eb6EBcfA62792A01E5005c453F39D63493a79B8", "0", "1");
   await sbt.mint("0x3128ef7F0933cF2bA18f1Ef7280A7b684347B115", "0", "1");
+
+  await sbt.mint("0x4eb6EBcfA62792A01E5005c453F39D63493a79B8", "1", "233");
+  await sbt.mint("0x3128ef7F0933cF2bA18f1Ef7280A7b684347B115", "1", "432");
 
   console.log(`Soulbound deployed to ${sbt.target}`);
 }
